d0a09d79a2c964d74c6492b1b5f6c32b96b2b95a
sriman varma <srimannarayanavarmadunde@gmail.com>
2025-05-24 19:28:17 +0530

    added authmiddleware

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -4,7 +4,7 @@ import { User } from "../db"
 import { z } from "zod";
 import jwt from "jsonwebtoken";
 import { JWT_SECRET } from "../config";
-
+import { authMiddleware } from "../middleware";
 
 
 const userRouter = express.Router();
@@ -87,5 +87,31 @@ userRouter.post("/signin", async (req: Request, res: Response) => {
         message: "Error while logging in"
     })
 })
+
+const updateBody = z.object({
+    firstName: z.string(),
+    lastName: z.string(),
+    password: z.string()
+})
+
+//@ts-ignore
+userRouter.put("/",authMiddleware,async(req,res)=>{
+    const { success}= updateBody.safeParse(req.body);
+    if(!success){
+        return res.status(411).json({
+            message: "Error while updating the information"
+        })
+    }
+
+    //@ts-ignore
+    await User.updateOne({_id: req.userId},req.body);
+
+    res.json({
+        message: "Updated succefully"
+    })
+    
+
+
+});
 
 export default userRouter;
